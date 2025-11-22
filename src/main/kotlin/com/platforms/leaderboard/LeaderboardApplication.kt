1545fb82358c33619fe6c435660d312764c20be4
rhythmgt <akansh@Akanshs-MacBook-Air.local>
2025-11-22 14:46:40 +0530

    rhythm | project init

---
+++ src/main/kotlin/com/platforms/leaderboard/LeaderboardApplication.kt
@@ -0,0 +1,11 @@
+package com.platforms.leaderboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeaderboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeaderboardApplication>(*args)
+}
---
+++ src/test/kotlin/com/platforms/leaderboard/LeaderboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.platforms.leaderboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeaderboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
